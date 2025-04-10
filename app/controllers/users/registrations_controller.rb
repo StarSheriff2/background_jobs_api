36e543c07582ed441d1b532a96607df2f60ff979
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2025-04-10 12:36:32 -0600

    setup welcome email job

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
+
+  def create
+    super do |user|
+      SendWelcomeEmailJob.perform_later(user.id)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  before_create :set_jti
+
+  private
+
+  def set_jti
+    self.jti ||= SecureRandom.uuid
+  end
 end
