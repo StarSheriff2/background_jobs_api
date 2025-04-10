f4d24ec89fd2dcbe093ecf089f7c6d67e4dae3d3
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2025-04-10 11:53:59 -0600

    enable middleware for application

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module BackgroundJobsApi
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
