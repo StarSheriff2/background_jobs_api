a5f0d3fbb3f241ceed88ce47f1795d2d17c05d2f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2025-04-28 19:58:48 -0600

    chore: linter

--- app/jobs/scrape_and_summarize_job.rb
+++ app/jobs/scrape_and_summarize_job.rb
@@ -1,4 +1,4 @@
-require 'open-uri'
+require "open-uri"
 
 class ScrapeAndSummarizeJob < ApplicationJob
   queue_as :default
@@ -6,7 +6,7 @@ class ScrapeAndSummarizeJob < ApplicationJob
   def perform(url)
     html = URI.open(url).read
     doc = Nokogiri::HTML(html)
-    paragraphs = doc.css('p').map(&:text).join(' ')
+    paragraphs = doc.css("p").map(&:text).join(" ")
     summary = summarize(paragraphs)
     Rails.logger.info("Summary: #{summary}")
   end
