cc7f3656bd0367b41c5689ce1ede75858af09c5e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2025-05-06 09:58:26 -0600

    spec: add test to check extractor's article whitespace ignoring

--- spec/services/articles/extractor_spec.rb
+++ spec/services/articles/extractor_spec.rb
@@ -2,30 +2,53 @@ require 'rails_helper'
 
 RSpec.describe Articles::Extractor, type: :service do
   let(:url) { "http://example.com/article" }
-  let(:html_content) do
-    <<-HTML
-      <html lang="en">
-        <body>
-          <div class="article">
-            <p>First paragraph of the article.</p>
-            <p>Second paragraph of the article.</p>
-            <p>Related articles</p>
-          </div>
-        </body>
-      </html>
-    HTML
-  end
 
-  before do
+  def mock_html_content(html_content)
     allow(URI).to receive(:open).with(url).and_return(StringIO.new(html_content))
   end
 
   describe ".call" do
-    it "extracts and formats the article text" do
-      result = described_class.call(url)
+    context "when extracting and formatting article text" do
+      it "returns the cleaned article text" do
+        html_content = <<-HTML
+          <html lang="en">
+            <body>
+              <div class="article">
+                <p>First paragraph of the article.</p>
+                <p>Second paragraph of the article.</p>
+                <p>Related articles</p>
+              </div>
+            </body>
+          </html>
+        HTML
 
-      expect(result.success?).to be true
-      expect(result.payload).to eq("First paragraph of the article.Second paragraph of the article.")
+        mock_html_content(html_content)
+        result = described_class.call(url)
+
+        expect(result.success?).to be true
+        expect(result.payload).to eq("First paragraph of the article.Second paragraph of the article.")
+      end
+    end
+
+    context "when paragraphs are preceded by 3 or more consecutive whitespace characters" do
+      it "discards those paragraphs" do
+        html_content_with_whitespace = <<-HTML
+          <html lang="en">
+            <body>
+              <div class="article">
+                <p>First paragraph of the article.\n\n\n</p>
+                <p>Second paragraph of the article.</p>
+              </div>
+            </body>
+          </html>
+        HTML
+
+        mock_html_content(html_content_with_whitespace)
+        result = described_class.call(url)
+
+        expect(result.success?).to be true
+        expect(result.payload).to eq("First paragraph of the article.")
+      end
     end
   end
 end
